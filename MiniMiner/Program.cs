db1026003fc609b40dcbef413ee668288ddaacbf
severin.amacher <severin.amacher@gmx.net>
2025-01-30 21:22:18 +0100

    fix repository structure

--- MiniMiner/Program.cs
+++ MiniMiner/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using MiniMiner;
+
+var testString =
+    "{\"difficulty\": 8, \"block\": {\"nonce\": null, \"data\": []}}";
+
+var options = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+var minerObject = JsonSerializer.Deserialize<MiniMinerJson>(testString, options);
+
+if (minerObject is null)
+{
+    Console.WriteLine("Json can not be deserialized.");
+    return;
+}
+
+Console.WriteLine(minerObject.GetCorrectHashString());
+Console.WriteLine($"Nonce: {minerObject.Block.Nonce}");
